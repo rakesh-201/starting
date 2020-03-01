576fa21d8de752bb41eeb5aa6112b7d2c6b6c9aa
RAKESH <rakesh.rajpurohit.rr.rr.201@gmail.com>
2020-03-01 11:04:34 +0530

    done

--- rak.cpp
+++ rak.cpp
@@ -5,3 +5,18 @@ hbl;jyjyiurjyyljbjrqliybuy;ojljb;biy
 lkhrlijljre;mbjb3
 ljncdiywkunfcfh
 wdicyw
+#include<ioostream>
+using namespace std;
+int main ()
+{
+	#include"
+	
+	return 0;
+}
+#include<iostream>
+using namespace std;
+int main(){
+	
+	return 0;
+}
+	
